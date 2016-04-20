1d8bfa2ed61aa242d543fd7772df52033e8d6d3f
Syntic <synticus@gmail.com>
2016-04-20 17:24:39 +0200

    Working login()

--- InfoGrab.py
+++ InfoGrab.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
 import os
 
 # def scan():
@@ -17,35 +19,20 @@ import os
 #
 # scan()
 
-def login(page, server, domain):
+def login(page, server, domain, username, password):
 
     # Generate new URL
-    # url = 'http://' + server + '.travian.' + domain + '/' + page
-    # print(url)
+    url = 'http://' + server + '.travian.' + domain + '/' + page
 
     # Open session & add user agent
     s = requests.session()
     s.headers.update({'user-agent': 'Mozilla/5.0'})
 
     # Set Login Data
-    login_data = dict(name='Synticus', password='travianpw', s1='Einloggen')
-
+    login_data = dict(name=username, password=password, s1='Einloggen', login=time.time())
     # Send login data to login page
-    l = s.post('http://ts3.travian.de/login.php', data=login_data)
-    print(l.status_code)
-    print(l.content)
+    s.post(url, data=login_data)
+    # get page after login
+    r = s.get(url)
 
-    postcont = BeautifulSoup(l.content, "lxml")
-    print(postcont.prettify())
-
-    # print content that should be available off a page after login
-    r = s.get('http://ts3.travian.de/dorf1.php')
-
-    # print(r.status_code)
-    # print('----------------------------------------')
-    # print(r.content)
-    #
-    # soup = BeautifulSoup(r.content, "lxml")
-    # print(soup.prettify())
-
-login('dorf2.php', 'ts3', 'de')
+login('dorf1.php', 'ts3', 'de', 'Synticus', 'travianpw')
