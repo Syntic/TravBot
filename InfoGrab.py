3a81e53b832859c13ad92c1d5bc3d83557af9ad0
Syntic <synticus@gmail.com>
2016-04-22 01:25:22 +0200

    Progress for get_dorf1:
    - Added Resource current/max/production values
    - Added mapping + level of fields

--- InfoGrab.py
+++ InfoGrab.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import re
 
 def set_server(server,domain):
     # Generate new URL
@@ -23,12 +24,63 @@ def login(page, server, username, password):
     # return session
     return s
 
-def get_page(server, page, session):
-    text = session.get(server + '/' + page)
+def get_dorf1(session, server):
 
+    # Grab dorf1.php
+    text = session.get(server + '/' + 'dorf1.php')
+    # Into BS
+    soup = BeautifulSoup(text.content, 'lxml')
+
+    ### Resources and Productions
+
+    #Search for the resources block
+    for hit in soup.find_all('script'):
+        #Convert list to string
+        str = ''.join(hit.contents)
+        #See if string cotains the word resources
+        if re.search("resources", str):
+
+            #remove surrounding stuff
+            str = str.replace(",", " ");
+            str = str.replace("\"", " ");
+            str = str.replace(":", " ");
+
+            #Just take digits
+            resources = [int(s) for s in str.split() if s.isdigit()]
+            print('Resources:')
+            print(resources)
+
+    #Map Resource Fields + Levels
+
+    currentfield = 1
+
+    for hit in soup.find_all('area'):
+
+        if "Holzfäller" in hit.get('title'):
+            print('ID', currentfield, 'Holz')
+            currentfield += 1
+
+        if "Getreidefarm" in hit.get('title'):
+            print('ID', currentfield, 'Getreide')
+            currentfield += 1
+
+        if "Eisenmine" in hit.get('title'):
+            print('ID', currentfield, 'Eisen')
+            currentfield += 1
+
+        if "Lehmgrube" in hit.get('title'):
+            print('ID', currentfield, 'Lehm')
+            currentfield += 1
+
+        #New Subsoup for level
+        subsoup = BeautifulSoup(hit.get('title'), 'lxml')
+        #Cut to only relevant String
+        result = subsoup.find(class_="level")
+        print(result.string)
+        if currentfield == 19:
+            break
 
 
 server = set_server('ts3', 'de')
 session = login('dorf1.php', server, 'Synticus', 'travianpw')
-text = session.get(server + '/' + 'dorf1.php')
-print(text.content)
+get_dorf1(session, server)
